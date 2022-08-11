7a804bcd2dcb3c1d69153f64c91fccf7c1558593
Daniel Cazzulino <daniel@cazzulino.com>
2022-08-11 14:47:46 -0300

    Allow including directly from URLs
    Detects if the included path is a uri, and if so, fetches it before applying the existing behavior for fragment lookup.
    
    Refactor code into a separate file for easier local testing.
    
    Fixes #9

--- readme.md
+++ readme.md
@@ -51,7 +51,7 @@ You can exclude a file from processing by having as the first (or last) line:
     include: ''
 
     # files to exclude from processing, i.e. 'header.md'
-    # Default: $null
+    # Default: ''
     exclude: ''
 
     # whether to recurse into subdirectories
@@ -150,17 +150,17 @@ be detected).
   running the action multiple times with different sets of files.
 
 
-<!-- include docs/footer.md -->
-## Sponsors 
+<!-- include https://github.com/devlooped/sponsors/raw/main/footer.md -->
+# Sponsors 
 
 <!-- sponsors.md -->
-[![Kirill Osenkov](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/KirillOsenkov.png "Kirill Osenkov")](https://github.com/KirillOsenkov)
-[![C. Augusto Proiete](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/augustoproiete.png "C. Augusto Proiete")](https://github.com/augustoproiete)
-[![SandRock](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/sandrock.png "SandRock")](https://github.com/sandrock)
-[![Amazon Web Services](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/aws.png "Amazon Web Services")](https://github.com/aws)
-[![Christian Findlay](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/MelbourneDeveloper.png "Christian Findlay")](https://github.com/MelbourneDeveloper)
 [![Clarius Org](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/clarius.png "Clarius Org")](https://github.com/clarius)
+[![Christian Findlay](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/MelbourneDeveloper.png "Christian Findlay")](https://github.com/MelbourneDeveloper)
+[![C. Augusto Proiete](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/augustoproiete.png "C. Augusto Proiete")](https://github.com/augustoproiete)
+[![Kirill Osenkov](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/KirillOsenkov.png "Kirill Osenkov")](https://github.com/KirillOsenkov)
 [![MFB Technologies, Inc.](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/MFB-Technologies-Inc.png "MFB Technologies, Inc.")](https://github.com/MFB-Technologies-Inc)
+[![Amazon Web Services](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/aws.png "Amazon Web Services")](https://github.com/aws)
+[![SandRock](https://raw.githubusercontent.com/devlooped/sponsors/main/.github/avatars/sandrock.png "SandRock")](https://github.com/sandrock)
 
 
 <!-- sponsors.md -->
@@ -170,4 +170,4 @@ be detected).
 
 [Learn more about GitHub Sponsors](https://github.com/sponsors)
 
-<!-- docs/footer.md -->
+<!-- https://github.com/devlooped/sponsors/raw/main/footer.md -->
---
+++ resolve-file-includes.ps1
@@ -0,0 +1,90 @@
+$include = $env:RESOLVE_INCLUDE ?? '*.md'
+$exclude = $env:RESOLVE_EXCLUDE ?? $null
+$recurse = [bool]$env:RESOLVE_RECURSE
+$validate = [bool]$env:RESOLVE_VALIDATE
+
+$files = if ($recurse) {
+  Get-ChildItem -Include $include -Exclude $exclude -Recurse
+} else {
+  Get-ChildItem -Include $include -Exclude $exclude
+}
+
+foreach ($file in $files) {
+    Write-Output "Processing $file"
+    $content = ((Get-Content $file -Raw) ?? '').Trim()
+    if ($content.StartsWith('<!-- exclude -->') -or $content.EndsWith('<!-- exclude -->')) {
+        Write-Output "Excluding $file"
+        continue
+    }
+
+    $replacements = @{}
+    foreach ($match in (Select-String -Pattern '<!--\s?include (.*?)\s?-->' -Path $file)) {
+        $includedPath = ($match.Matches[0].Value -replace '<!--\s?include ','' -replace '\s?-->', '').Trim()
+        $fragment = $null
+        if ($includedPath.Contains('#')) {
+            $fragment = '#' + $includedPath.Split('#')[1]
+            $includedPath = $includedPath.Substring(0, $includedPath.IndexOf('#'))
+        }
+
+        $isuri = [uri]::IsWellFormedUriString($includedPath, 'Absolute')
+        $includedFullPath = Join-Path (Get-ChildItem -Path $file).DirectoryName -ChildPath $includedPath
+
+        if ((Test-Path $includedFullPath) -Or $isuri) {
+            $include = if ($isuri) {
+                (Invoke-RestMethod $includedPath) ?? ''
+            } else {
+                (Get-Content $includedFullPath -Raw) ?? ''
+            }
+
+            # Resolve fragment specifier if present
+            if ($fragment) {
+                $anchor = "<!-- $fragment -->"
+                $start = $include.IndexOf($anchor)
+                if ($start -eq -1) {
+                    if ($validate) {
+                        Write-Error "Referenced anchor $fragment not found in $includedPath"
+                    } else {
+                        Write-Warning "Referenced anchor $fragment not found in $includedPath"
+                    }
+                    continue
+                }
+                $include = $include.Substring($start)
+                $end = $include.IndexOf($anchor, $anchor.Length)
+                if ($end -ne -1) {
+                    $include = $include.Substring(0, $end + $anchor.Length)
+                }
+            }
+            # TODO: disable nested includes until we can properly support that scenario, and nested excludes
+            $include = $include -replace '<!--\s?include ','<!-- ' -replace '<!-- exclude -->', ''
+            # see if we already have a section we previously replaced
+            $existingRegex = "<!--\s?include $includedPath$fragment\s?-->[\s\S]*<!-- $includedPath$fragment -->"
+            $replacement = "<!-- include $includedPath$fragment -->`n$include`n<!-- $includedPath$fragment -->"
+            if ($content -match $existingRegex) {
+                $replacements[$existingRegex] = $replacement
+            } else {
+                $replacements["<!--\s?include $includedPath$fragment\s?-->"] = $replacement
+            }
+        } else {
+            if ($validate) {
+                Write-Error "Included file $includedPath in $($file.Name) not found" 
+            } else {
+                Write-Warning "Included file $includedPath in $($file.Name) not found" 
+            }
+        }
+    }
+
+    if ($replacements.Count -gt 0) {
+        foreach ($replacement in $replacements.GetEnumerator()) {
+            #Write-Host "Replacing $($replacement.Key) with $($replacement.Value)"
+            $content = $content -replace $replacement.Key, $replacement.Value
+        }
+
+        $content = $content.TrimEnd()
+        $actual = (Get-Content $file -Raw).TrimEnd()
+        
+        if ($content -ne $actual) {
+            Set-Content $file -Value $content.TrimEnd()
+            Write-Output "Updated $($file.Name)"
+        }
+    }
+}
